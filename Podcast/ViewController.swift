16e832e5755119c6f862b28365767ffe92993d0a
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-11-13 00:29:21 +0100

    Initial Commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Raidan Shugaa Addin on 2022. 11. 13..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
