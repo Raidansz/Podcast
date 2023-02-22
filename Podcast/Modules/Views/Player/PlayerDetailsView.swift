8f59217375191bfc7453724af69f6125a4fec5e4
raidan <raidancia0@gmail.com>
2023-02-22 23:29:34 +0100

    fixing some bugs

--- Podcast/Modules/Views/Player/PlayerDetailsView.swift
+++ Podcast/Modules/Views/Player/PlayerDetailsView.swift
@@ -57,33 +57,60 @@ class PlayerDetailsView:UIView{
         if player.timeControlStatus == .paused{
             player.play()
             playPauseButtonOutlet.setImage(UIImage(systemName: "pause.fill"), for: .normal)
-            imageIfPlayed()
+        enlargeEpisodeImage()
         }else{
             player.pause()
             playPauseButtonOutlet.setImage(UIImage(systemName: "play.fill"), for: .normal)
-            imageIfPaused()
+            shrinkEpisodeImage()
         }
        
        
     }
     
-    func imageIfPlayed(){
+    func enlargeEpisodeImage(){
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1) {
             self.episodeImage.transform = .identity
         }
     }
     
     
-    func imageIfPaused(){
+    func shrinkEpisodeImage(){
         let scale:CGFloat = 0.7
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1) {
             self.episodeImage.transform = CGAffineTransform(scaleX: scale, y: scale)
         }
     }
-    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //TODO
+//       let time = CMTimeMake(value: 1, timescale: 3)
+//        let times = [NSValue(time: time)]
+////        player.addBoundaryTimeObserver(forTimes: times, queue: .main) {
+////            self.enlargeEpisodeImage()
+////            print("started plattitienginag")
+//       // }
+//
+//
+//            // Set initial time to zero
+//
+//            // Divide the asset's duration into quarters.
+//
+//
+//            // Build boundary times at 25%, 50%, 75%, 100%
+//
+//
+//            // Add time observer. Observe boundary time changes on the main queue.
+//            player.addBoundaryTimeObserver(forTimes: times,
+//                                                               queue: .main) { [weak self] in
+//                // Update UI
+//                self?.enlargeEpisodeImage()
+//            }
+    }
     
     @IBOutlet weak var episodeImage: UIImageView!{
         didSet{
+            let scale:CGFloat = 0.7
+            episodeImage.transform = CGAffineTransform(scaleX: scale, y: scale)
             episodeImage.layer.cornerRadius = 10
             episodeImage.layer.masksToBounds = true
         }
@@ -92,7 +119,10 @@ class PlayerDetailsView:UIView{
     @IBOutlet weak var authorLabel: UILabel!
     
     @IBAction func handleDismiss(_ sender: Any) {
-        self.removeFromSuperview()
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1) {
+            self.removeFromSuperview()
+        }
+       
     }
     
     
