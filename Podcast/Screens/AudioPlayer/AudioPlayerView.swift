d1cb1e798f60982e6b3c6e6b9d4de8e44ad7536f
Raidan <92235772+Raidansz@users.noreply.github.com>
2024-08-05 22:28:36 +0200

    remove some trailing whitespaces

--- Podcast/Screens/AudioPlayer/AudioPlayerView.swift
+++ Podcast/Screens/AudioPlayer/AudioPlayerView.swift
@@ -11,7 +11,6 @@ import AVKit
 
 struct AudioPlayerView: View {
     @StateObject var viewModel: AudioPlayerViewModel
-    
     var body: some View {
         VStack {
             ZStack {
@@ -20,13 +19,11 @@ struct AudioPlayerView: View {
                     Spacer()
                     ModifiedButtonView(image: "line.horizontal.3.decrease")
                 }
-                
                 Text("Now Playing")
                     .fontWeight(.bold)
                     .foregroundColor(.black.opacity(0.8))
             }
             .padding(.all)
-            
             Image("tree")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
@@ -38,18 +35,18 @@ struct AudioPlayerView: View {
                 .shadow(color: Color.black.opacity(0.35), radius: 8, x: 8, y: 8)
                 .shadow(color: Color.white, radius: 10, x: -10, y: -10)
                 .padding(.top, 35)
-            
+
             Text("Drift")
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(.black.opacity(0.8))
                 .padding(.top, 25)
-            
+
             Text("Robot Koch ft. nilu")
                 .font(.caption)
                 .foregroundColor(.black.opacity(0.8))
                 .padding(.top, 2)
-            
+
             VStack {
                 HStack {
                     Text(timeString(time: viewModel.currentTime))
@@ -59,7 +56,7 @@ struct AudioPlayerView: View {
                 .font(.caption)
                 .foregroundColor(.black.opacity(0.8))
                 .padding([.top, .trailing, .leading], 20)
-                
+
                 Slider(value: Binding(get: {
                     viewModel.currentTime
                 }, set: { newValue in
@@ -67,12 +64,12 @@ struct AudioPlayerView: View {
                 }), in: 0...viewModel.totalTime)
                 .padding([.top, .trailing, .leading], 20)
             }
-            
+
             HStack(spacing: 20) {
                 Button(action: {}, label: {
                     ModifiedButtonView(image: "backward.fill")
                 })
-                
+
                 Button {
                     viewModel.isPlaying ? viewModel.pauseAudio() : viewModel.playAudio()
                 } label: {
@@ -83,12 +80,12 @@ struct AudioPlayerView: View {
                         .background(
                             ZStack {
                                 Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
-                                
+
                                 Circle()
                                     .foregroundColor(.white)
                                     .blur(radius: 4)
                                     .offset(x: -8, y: -8)
-                                
+
                                 Circle()
                                     .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8980392157, green: 0.933333333, blue: 1, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing))
                                     .padding(2)
@@ -98,19 +95,19 @@ struct AudioPlayerView: View {
                                 .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
                                 .shadow(color: Color.white, radius: 20, x: -20, y: -20))
                 }
-                
+
                 Button(action: {}, label: {
                     ModifiedButtonView(image: "forward.fill")
                 })
             }
             .padding(.top, 25)
-            
+
             Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
     }
-    
+
     private func timeString(time: TimeInterval) -> String {
         let minute = Int(time) / 60
         let seconds = Int(time) % 60
